e94e7f4db1b9652ffba581f20597c6d44200057f
Rick Jensen <cdeszaq@yahoo-inc.com>
2016-09-13 09:55:01 -0500

    Add verify publication steps and fail on errors.
    Also add severity levels to log lines emitted by scripts and condense
    code to group operations a bit more.

--- travis/deploy.bash
+++ travis/deploy.bash
@@ -4,38 +4,51 @@ RELEASE_TAG_REGEX="^[0-9]+\\.[0-9]+\\.[0-9]+"
 
 # Deploy if this has a travis tag and is on an approved branch
 MATCHING_TAG=$(echo $TRAVIS_TAG | egrep ${RELEASE_TAG_REGEX})
-
 if [[ ${MATCHING_TAG} != "" ]]; then
     # This is a tag event with a version style tag
-    echo "Publishing, this is a tag event: ${TRAVIS_TAG}"
+    echo "INFO Publishing, this is a tag event: ${TRAVIS_TAG}"
 
-    echo "Maven versions set : "
+    echo "INFO Maven versions set : "
     mvn versions:set -DnewVersion=$(git describe) -DgenerateBackupPoms=false
+    MAVEN_RETURN_CODE=$?
+    if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
+        echo "ERROR Unable to set version via Maven. Aborting publication."
+        exit ${MAVEN_RETURN_CODE}
+    fi
 
-    echo "Deploying: "
+    echo "INFO Deploying: "
     mvn deploy --settings travis/bintray-settings.xml
+    MAVEN_RETURN_CODE=$?
+    if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
+        echo "ERROR Publication failed."
+        exit ${MAVEN_RETURN_CODE}
+    fi
+
+    # Publication succeeded. We're done here
     exit 0
 fi
 
+# We're not on a release tag, so build and test the code
 mvn verify
 MAVEN_RETURN_CODE=$?
-
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
-    echo "Maven did not succeed. Not flagging for publication."
+    echo "ERROR Maven did not succeed."
     exit ${MAVEN_RETURN_CODE}
 fi
 
+# Only publish whitelisted branches
 WHITELISTED=$(echo ${PUBLISH_WHITELIST} | grep ${TRAVIS_BRANCH})
 if [[ "${WHITELISTED}" == "" ]]; then
-    echo "Do not flag for publication, this is not a whitelisted branch: ${TRAVIS_BRANCH}"
+    echo "INFO Do not flag for publication, this is not a whitelisted branch: ${TRAVIS_BRANCH}"
     exit 0
 fi
 
+# Don't publish PR builds
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-    echo "Do not flag for publication, this is a PR: ${TRAVIS_PULL_REQUEST}"
-   exit 0
+    echo "INFO Do not flag for publication, this is a PR: ${TRAVIS_PULL_REQUEST}"
+    exit 0
 fi
 
-# This is a deployable push, update the tag to trigger a deploy
+# This is a publishable push, update the tag to trigger a deploy
 echo "Ci tagging: "
 travis/ci-tag.bash
