c82151e9552aba0bae73771b0d02fb939415ee76
Rick Jensen <cdeszaq@yahoo-inc.com>
2017-02-03 12:17:36 -0600

    Remove now duplicated tests for VersionHealthCheck

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/web/filters/HealthCheckFilterSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/web/filters/HealthCheckFilterSpec.groovy
@@ -6,9 +6,6 @@ import com.yahoo.bard.webservice.application.HealthCheckRegistryFactory
 import com.yahoo.bard.webservice.application.HealthCheckServletContextListener
 import com.yahoo.bard.webservice.application.JerseyTestBinder
 import com.yahoo.bard.webservice.application.ResourceConfig
-import com.yahoo.bard.webservice.application.healthchecks.VersionHealthCheck
-import com.yahoo.bard.webservice.config.SystemConfig
-import com.yahoo.bard.webservice.config.SystemConfigProvider
 import com.yahoo.bard.webservice.web.endpoints.DataServlet
 
 import com.codahale.metrics.health.HealthCheck
@@ -18,17 +15,10 @@ import spock.lang.Specification
 import spock.lang.Timeout
 
 import javax.ws.rs.ServiceUnavailableException
-import javax.ws.rs.container.ContainerRequestContext
-import javax.ws.rs.core.Response
 import javax.ws.rs.core.Response.Status
-import javax.ws.rs.core.UriInfo
 
 @Timeout(30)    // Fail test if hangs
 class HealthCheckFilterSpec extends Specification {
-
-    private static SystemConfig systemConfig = SystemConfigProvider.getInstance()
-
-    String originalVersionKey = systemConfig.getStringProperty(VersionHealthCheck.VERSION_KEY)
 
     JerseyTestBinder jtb = new JerseyTestBinder(
             HealthCheckFilter,
@@ -43,7 +33,6 @@ class HealthCheckFilterSpec extends Specification {
     }
 
     def cleanup() {
-        systemConfig.resetProperty(VersionHealthCheck.VERSION_KEY, originalVersionKey)
         cleanHealthCheckRegistry(registry)
 
         // Release the test web container
@@ -126,78 +115,6 @@ class HealthCheckFilterSpec extends Specification {
         expect:
         filter.filter(requestContext)
         theResponse == null
-    }
-
-    /* tests for VersionHealthCheck */
-
-    def "Healthly version check passes"() {
-        setup:
-        // Set expected values
-        String oldVersion = systemConfig.getStringProperty(VersionHealthCheck.VERSION_KEY, null)
-        String oldGitSha = systemConfig.getStringProperty(VersionHealthCheck.GIT_SHA_KEY, null)
-        systemConfig.setProperty(VersionHealthCheck.VERSION_KEY, "1.2.3.4")
-        systemConfig.setProperty(VersionHealthCheck.GIT_SHA_KEY, "b5d0be3956a18e128ff4192ca150f329f5e8f5c1")
-
-        // register healthy version health check
-        HealthCheckRegistry registry = HealthCheckRegistryFactory.getRegistry()
-        systemConfig.setProperty(VersionHealthCheck.VERSION_KEY, "1.2.3.4")
-        registry.register("VersionHealthCheck", new VersionHealthCheck() )
-
-        expect:
-        def healthCheck = registry.runHealthCheck("VersionHealthCheck")
-        healthCheck.getMessage() == "1.2.3.4:b5d0be3956a18e128ff4192ca150f329f5e8f5c1"
-        healthCheck.isHealthy()
-
-        and: "get /data passes in presence of VersionHealthCheck"
-        jtb.getHarness().target("data/shapes/day/color")
-            .queryParam("metrics","width")
-            .queryParam("dateTime","2014-06-11%2F2014-06-12")
-            .request().get(String.class)
-
-        cleanup:
-        systemConfig.resetProperty(VersionHealthCheck.VERSION_KEY, oldVersion)
-        systemConfig.resetProperty(VersionHealthCheck.GIT_SHA_KEY, oldGitSha)
-    }
-
-    def "Unset version check fails"() {
-        setup: "register unhealthy version health check"
-
-        // Get the registry
-        HealthCheckRegistry registry = HealthCheckRegistryFactory.getRegistry()
-
-        // Get the configuration, stash any old value to restore later, and set a known value
-        String propertyName = "package_name"
-        String oldProperty = systemConfig.getStringProperty(propertyName)
-
-        systemConfig.setProperty(propertyName, "test2")
-
-        // Re-register this health check to make sure it's clean and under our control
-        String registeredName = "VersionHealthCheck"
-        registry.unregister(registeredName)
-        registry.register(
-                registeredName,
-                new VersionHealthCheck(
-                        systemConfig.getPackageVariableName("version"),
-                        systemConfig.getPackageVariableName("gitsha")
-                )
-        )
-
-        expect: "The health check isn't healthy"
-        def healthCheck = registry.runHealthCheck(registeredName)
-        healthCheck.isHealthy() == false
-
-        and: "The message is what we expect"
-        healthCheck.getMessage() == "${VersionHealthCheck.VERSION_KEY} not set"
-
-        and: "get /data fails in presence of VersionHealthCheck"
-        Response r = jtb.getHarness().target("data/shapes/day/color")
-            .queryParam("metrics","width")
-            .queryParam("dateTime","2014-06-11%2F2014-06-12")
-            .request().get()
-        r.getStatus() == Status.SERVICE_UNAVAILABLE.getStatusCode()
-
-        cleanup: "Remove version health check and replace old property value"
-        systemConfig.resetProperty(propertyName, oldProperty)
     }
 
     private static void cleanHealthCheckRegistry(HealthCheckRegistry registry) {
