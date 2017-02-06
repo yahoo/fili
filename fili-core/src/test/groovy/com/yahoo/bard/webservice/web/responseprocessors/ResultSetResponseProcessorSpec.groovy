49d329de155f3a30d8cdad3b212f246ae038c72c
Michael McLawhorn <mclawhor@yahoo-inc.com>
2017-02-06 13:39:03 -0600

    Fixed ResultSetResponseProcessorSpec

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/web/responseprocessors/ResultSetResponseProcessorSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/web/responseprocessors/ResultSetResponseProcessorSpec.groovy
@@ -59,7 +59,12 @@ class ResultSetResponseProcessorSpec extends Specification {
     PathSegment pathSegment
     MultivaluedMap paramMap
 
+    String dimension1Name = "dimension1"
     Dimension d1
+    Dimension d2
+
+    String metric1Name = "agg1"
+    String metric2Name = "postagg1"
 
     ResultSetMapper rsm1
     LogicalMetric lm1
@@ -99,28 +104,33 @@ class ResultSetResponseProcessorSpec extends Specification {
         List<PostAggregation> postAggs = new ArrayList<Dimension>()
         d1 = Mock(Dimension)
         dimensions.add(d1)
-        d1.getApiName() >> "dimension1"
+        d1.getApiName() >> dimension1Name
+        Dimension d2 = Mock(Dimension)
+        dimensions.add(d2)
+        d2.getApiName() >> "dimension2"
+
         Aggregation agg1 = Mock(Aggregation)
         aggregations.add(agg1)
-        agg1.getName() >> "agg1"
+        agg1.getName() >> metric1Name
+        Aggregation agg2 = Mock(Aggregation)
+        agg2.getName() >> "otherAgg"
+        aggregations.add(agg2)
 
         PostAggregation postAgg1 = Mock(PostAggregation)
         postAggs.add(postAgg1)
-        postAgg1.getName() >> "postAgg1"
+        postAgg1.getName() >> metric2Name
+        PostAggregation postAgg2 = Mock(PostAggregation)
+        postAggs.add(postAgg2)
+        postAgg2.getName() >> "otherPostAgg"
+
 
         DefaultQueryType queryType = DefaultQueryType.GROUP_BY
         groupByQuery.getQueryType() >> queryType
         groupByQuery.getDimensions() >> dimensions
         groupByQuery.getAggregations() >> aggregations
         groupByQuery.getPostAggregations() >> postAggs
-        groupByQuery.getDataSource() >> new TableDataSource(new ConcretePhysicalTable("table_name", DAY.buildZonedTimeGrain(DateTimeZone.UTC), ["dimension1":"dimension1"]))
+        groupByQuery.getDataSource() >> new TableDataSource(new ConcretePhysicalTable("table_name", DAY.buildZonedTimeGrain(DateTimeZone.UTC), [] as Set, ["dimension1":"dimension1"]))
 
-        Dimension d = Mock(Dimension)
-        dimensions.add(d)
-        Aggregation agg = Mock(Aggregation)
-        aggregations.add(agg)
-        PostAggregation postAgg = Mock(PostAggregation)
-        postAggs.add(postAgg)
 
 
         ResultSetSchema schema = new ResultSetSchema(DAY, Sets.newHashSet(new MetricColumn("lm1")))
@@ -160,26 +170,32 @@ class ResultSetResponseProcessorSpec extends Specification {
                 httpResponseMaker
         )
         JsonNode jsonMock = Mock(JsonNode)
-        Schema captureSchema = null
+        ResultSetSchema captureSchema = null
         JsonNode captureJson
         ResultSet rs = Mock(ResultSet)
 
-        1 * druidResponseParser.parse(_, _, _) >> {
-            JsonNode json, Schema schema, DefaultQueryType type -> captureSchema = schema; captureJson = json; rs
+        1 * druidResponseParser.parse(_, _, _, _) >> {
+            JsonNode json, Schema schema, DefaultQueryType type, DateTimeZone dateTimeZone
+                ->
+                captureSchema = schema;
+                captureJson = json;
+                rs
         }
         ResultSet actual
 
         when:
         actual = resultSetResponseProcessor.buildResultSet(jsonMock, groupByQuery, DateTimeZone.UTC)
-        DimensionColumn dimCol = captureSchema.getColumn("dimension1")
-        MetricColumn m1 = captureSchema.getColumn("agg1")
-        MetricColumn m2 = captureSchema.getColumn("postAgg1")
+        DimensionColumn dimCol = captureSchema.getColumn(dimension1Name, DimensionColumn.class).get()
+        MetricColumn m1 = captureSchema.getColumn(metric1Name, MetricColumn.class).get()
+        MetricColumn m2 = captureSchema.getColumn(metric2Name, MetricColumn.class).get()
+        Optional<MetricColumn> m3 = captureSchema.getColumn("fakeMetric", MetricColumn.class)
 
         then:
         captureSchema.granularity == DAY
         dimCol.dimension == d1
         m1 != null
         m2 != null
+        m3 == Optional.empty()
         actual == rs
 
     }
