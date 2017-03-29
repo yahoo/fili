cd68c0373068f42ae7b091f6b3cf530db3d8fa2f
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-03-29 18:22:25 -0500

    Fix codacy complaints

--- travis/non_pull_request_setup.bash
+++ travis/non_pull_request_setup.bash
@@ -2,5 +2,5 @@
 git remote set-url origin git@github.com:yahoo/fili.git
 openssl aes-256-cbc -K $encrypted_3b73c80ce5a3_key -iv $encrypted_3b73c80ce5a3_iv -in github_key.enc -out github_key -d
 chmod 600 github_key
-eval `ssh-agent -s`
+eval $(ssh-agent -s)
 ssh-add github_key
