79d33ba82b616e76bedc59e555ebe12f4432ef7f
Rick Jensen <cdeszaq@yahoo-inc.com>
2017-03-30 16:24:47 -0500

    Move the git credit load into a conditional

--- travis/before_install.bash
+++ travis/before_install.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
-git remote set-url origin git@github.com:yahoo/fili.git
-openssl aes-256-cbc -K "${encrypted_3b73c80ce5a3_key}" -iv "${encrypted_3b73c80ce5a3_iv}" -in github_key.enc -out github_key -d
-chmod 600 github_key
-eval $(ssh-agent -s)
-ssh-add github_key
+
+# Load credentials for git when not a PR
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+    echo "INFO Setting up git credentials, this is not a PR: ${TRAVIS_PULL_REQUEST}"
+    git remote set-url origin git@github.com:yahoo/fili.git
+    openssl aes-256-cbc -K ${encrypted_3b73c80ce5a3_key} -iv ${encrypted_3b73c80ce5a3_iv} -in github_key.enc -out github_key -d
+    chmod 600 github_key
+    eval $(ssh-agent -s)
+    ssh-add github_key
+fi
