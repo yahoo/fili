50005a7a066b48eb2c2529cf4f18fb2ad7ce6ff9
Jiaqi Liu <QubitPi@users.noreply.github.com>
2017-04-28 09:12:28 -0700

    Add test for MetricUnionCompositeTableDefinition (#260)

---
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/table/MetricUnionCompositeTableDefinitionSpec.groovy
@@ -0,0 +1,36 @@
+package com.yahoo.bard.webservice.table
+
+import com.yahoo.bard.webservice.data.config.ResourceDictionaries
+import com.yahoo.bard.webservice.data.config.names.TableName
+import com.yahoo.bard.webservice.data.config.table.MetricUnionCompositeTableDefinition
+import com.yahoo.bard.webservice.data.time.ZonedTimeGrain
+import com.yahoo.bard.webservice.data.time.ZonedTimeGrainSpec
+
+import spock.lang.Specification
+
+class MetricUnionCompositeTableDefinitionSpec extends Specification {
+    
+    def "getPhysicalTables filters out dependent tables that does not exist in ResourceDictionaries"() {
+        given:
+        TableName name = TableName.of('table')
+        ZonedTimeGrain timeGrain = ZonedTimeGrainSpec.DAY_UTC
+
+        TableName existing = TableName.of('existing')
+        TableName nonExisting = TableName.of('nonExisting')
+
+        MetricUnionCompositeTableDefinition metricUnionCompositeTableDefinition = new MetricUnionCompositeTableDefinition(
+                name,
+                timeGrain,
+                [] as Set,
+                [existing, nonExisting] as Set,
+                [] as Set
+        )
+
+        PhysicalTable physicalTable = Mock(PhysicalTable)
+        ResourceDictionaries resourceDictionaries = Mock(ResourceDictionaries)
+        resourceDictionaries.getPhysicalDictionary() >> [(existing.asName()) : physicalTable]
+
+        expect:
+        metricUnionCompositeTableDefinition.getPhysicalTables(resourceDictionaries) == [physicalTable] as Set
+    }
+}
