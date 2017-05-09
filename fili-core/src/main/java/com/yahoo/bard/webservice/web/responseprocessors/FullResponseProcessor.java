3d5d8f58eb35dc0045f27802cfefe2d91c7d81b2
Jiaqi Liu <2257440489@qq.com>
2017-05-09 13:26:30 -0700

    delete

--- fili-core/src/main/java/com/yahoo/bard/webservice/web/responseprocessors/FullResponseProcessor.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 Yahoo Inc.
-// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
-package com.yahoo.bard.webservice.web.responseprocessors;
-
-public interface FullResponseProcessor extends ResponseProcessor {
-
-}
