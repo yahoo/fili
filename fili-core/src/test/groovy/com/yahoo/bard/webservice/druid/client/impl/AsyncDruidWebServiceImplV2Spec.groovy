dc3cff7933545f094be0bf05bec4a63f03ea5f38
Jiaqi Liu <2257440489@qq.com>
2017-05-12 09:41:03 -0700

    address @garyluoex's comments

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/druid/client/impl/AsyncDruidWebServiceImplV2Spec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/druid/client/impl/AsyncDruidWebServiceImplV2Spec.groovy
@@ -16,7 +16,7 @@ class AsyncDruidWebServiceImplV2Spec extends Specification {
     def "Make sure X-Druid-Response-Context and status-code are merged into existing JsonNode"() {
         given:
         Response response = Mock(Response)
-        response.getResponseBodyAsStream() >> new ByteArrayInputStream('{"k1":"v1"}'.getBytes(StandardCharsets.UTF_8))
+        response.getResponseBodyAsStream() >> new ByteArrayInputStream('[{"k1":"v1"}]'.getBytes(StandardCharsets.UTF_8))
         response.getHeader("X-Druid-Response-Context") >>
                 '''
                     {
@@ -41,7 +41,7 @@ class AsyncDruidWebServiceImplV2Spec extends Specification {
         asyncDruidWebServiceImplV2.constructJsonResponse(response).toString().replaceAll("\\\\", "") ==
                 '''
                     {
-                        "response": "{"k1":"v1"}",
+                        "response": "[{"k1":"v1"}]",
                         "X-Druid-Response-Context": "{
                             "uncoveredIntervals": [
                                 "2016-11-22T00:00:00.000Z/2016-12-18T00:00:00.000Z",
