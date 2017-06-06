06c89b5b2a7036c8a6bda26bfc0f090fea78b7b0
Gary Luo <weijialuo@yahoo-inc.com>
2017-06-06 10:44:48 -0500

    Add dimension dictionary to metric loader without breaking

--- fili-core/src/main/java/com/yahoo/bard/webservice/data/config/metric/MetricLoader.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/data/config/metric/MetricLoader.java
@@ -2,6 +2,7 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.data.config.metric;
 
+import com.yahoo.bard.webservice.data.dimension.DimensionDictionary;
 import com.yahoo.bard.webservice.data.metric.MetricDictionary;
 
 /**
@@ -16,4 +17,13 @@ public interface MetricLoader {
      */
     void loadMetricDictionary(MetricDictionary metricDictionary);
 
+    /**
+     * Load metrics and populate the metric dictionary with dimension dictionary for dimension dependent metrics.
+     *
+     * @param metricDictionary  The dictionary that will be loaded with metrics
+     * @param dimensionDictionary  The dimension dictionary containing loaded dimensions
+     */
+    default void loadMetricDictionary(MetricDictionary metricDictionary, DimensionDictionary dimensionDictionary) {
+        loadMetricDictionary(metricDictionary);
+    }
 }
