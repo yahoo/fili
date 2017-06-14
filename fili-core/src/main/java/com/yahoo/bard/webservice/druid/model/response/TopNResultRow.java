51cf3db20ffe50d103513105646b6468cbaa13b2
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-06-14 12:10:10 -0500

    Remove unused imports

--- fili-core/src/main/java/com/yahoo/bard/webservice/druid/model/response/TopNResultRow.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/druid/model/response/TopNResultRow.java
@@ -2,8 +2,6 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.druid.model.response;
 
-import com.yahoo.bard.webservice.druid.model.response.DruidResultRow;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
