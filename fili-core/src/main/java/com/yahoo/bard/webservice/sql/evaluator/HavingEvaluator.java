f8ee2194dee16b588ed0460bc15bae3088729610
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-06-26 16:35:18 -0500

    Partial implementation of a sql backend.
    - Generates/Evaluates Having, Filters, and PostAggregations (with tests)
    - Generates aggregations for sql
    - Minor helper code for making connections and building sql queries
    - Helper code for making tests quickly

---
+++ fili-core/src/main/java/com/yahoo/bard/webservice/sql/evaluator/HavingEvaluator.java
@@ -0,0 +1,178 @@
+// Copyright 2017 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.sql.evaluator;
+
+import com.yahoo.bard.webservice.druid.model.having.AndHaving;
+import com.yahoo.bard.webservice.druid.model.having.Having;
+import com.yahoo.bard.webservice.druid.model.having.MultiClauseHaving;
+import com.yahoo.bard.webservice.druid.model.having.NotHaving;
+import com.yahoo.bard.webservice.druid.model.having.NumericHaving;
+import com.yahoo.bard.webservice.druid.model.having.OrHaving;
+import com.yahoo.bard.webservice.sql.AliasMaker;
+
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlOperator;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.calcite.tools.RelBuilder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Evaluates Having filters on aggregated metrics to
+ * build an equivalent {@link RexNode} for making a sql query.
+ */
+public class HavingEvaluator {
+    /**
+     * Private constructor - all methods static.
+     */
+    private HavingEvaluator() {
+
+    }
+
+    /**
+     * Creates an {@link Optional} which contains the given {@link Having}
+     * as a {@link RexNode} or an empty value.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param having  The having filter being evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used in a sql query.
+     */
+    public static Optional<RexNode> buildFilter(
+            RelBuilder builder,
+            Having having,
+            AliasMaker aliasMaker
+    ) {
+        return Optional.ofNullable(evaluate(builder, having, aliasMaker));
+    }
+
+    /**
+     * Top level evaluate function which will call the correct "evaluate" method
+     * based on the having type.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param having  The having filter being evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used in a sql query.
+     */
+    private static RexNode evaluate(RelBuilder builder, Having having, AliasMaker aliasMaker) {
+        if (having == null) {
+            return null;
+        }
+        return DispatchUtils.dispatch(
+                HavingEvaluator.class,
+                "evaluate",
+                new Class[] {RelBuilder.class, having.getClass(), AliasMaker.class},
+                builder,
+                having,
+                aliasMaker
+        );
+    }
+
+    /**
+     * Evaluates a {@link NumericHaving}.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param having  The NumericHaving filter to be evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used in a sql query.
+     */
+    private static RexNode evaluate(
+            RelBuilder builder,
+            NumericHaving having,
+            AliasMaker aliasMaker
+    ) {
+        Having.DefaultHavingType havingType = (Having.DefaultHavingType) having.getType();
+        SqlOperator operator = null;
+        switch (havingType) {
+            case EQUAL_TO:
+                operator = SqlStdOperatorTable.EQUALS;
+                break;
+            case LESS_THAN:
+                operator = SqlStdOperatorTable.LESS_THAN;
+                break;
+            case GREATER_THAN:
+                operator = SqlStdOperatorTable.GREATER_THAN;
+                break;
+        }
+        return builder.call(
+                operator,
+                builder.field(aliasMaker.apply(having.getAggregation())),
+                builder.literal(having.getValue())
+        );
+    }
+
+    /**
+     * Evaluates a {@link NotHaving} filter.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param notHaving  The not having filter to be converted to be evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used in a sql query.
+     */
+    private static RexNode evaluate(RelBuilder builder, NotHaving notHaving, AliasMaker aliasMaker) {
+        return builder.call(
+                SqlStdOperatorTable.NOT,
+                evaluate(builder, notHaving.getHaving(), aliasMaker)
+        );
+    }
+
+    /**
+     * Evaluates a {@link OrHaving}.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param orHaving  The OrHaving to be evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used which ORs over the inner havings.
+     */
+    private static RexNode evaluate(RelBuilder builder, OrHaving orHaving, AliasMaker aliasMaker) {
+        return listEvaluate(builder, orHaving, SqlStdOperatorTable.OR, aliasMaker);
+    }
+
+    /**
+     * Evaluates a {@link AndHaving}.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param andHaving  The AndHaving to be evaluated.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used which ANDs over the inner havings.
+     */
+    private static RexNode evaluate(RelBuilder builder, AndHaving andHaving, AliasMaker aliasMaker) {
+        return listEvaluate(builder, andHaving, SqlStdOperatorTable.AND, aliasMaker);
+    }
+
+    /**
+     * Evaluates a {@link MultiClauseHaving} filter by performing it's operation over a list of other havings.
+     *
+     * @param builder  The RelBuilder used with Calcite to make queries.
+     * @param multiClauseHaving  The MultiClauseHaving filter to be evaluated.
+     * @param operator  The operator to be performed over the inner clauses of this having filter.
+     * @param aliasMaker  A function to get the aliased aggregation's name from the metric name.
+     *
+     * @return the equivalent {@link RexNode} to be used in a sql query.
+     */
+    private static RexNode listEvaluate(
+            RelBuilder builder,
+            MultiClauseHaving multiClauseHaving,
+            SqlOperator operator,
+            AliasMaker aliasMaker
+    ) {
+        List<RexNode> rexNodes = multiClauseHaving.getHavings()
+                .stream()
+                .map(having -> evaluate(builder, having, aliasMaker))
+                .collect(Collectors.toList());
+
+        return builder.call(
+                operator,
+                rexNodes
+        );
+    }
+}
---
+++ fili-core/src/main/java/com/yahoo/bard/webservice/sql/helper/CalciteHelper.java
@@ -0,0 +1,156 @@
+// Copyright 2017 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.sql.helper;
+
+import org.apache.calcite.adapter.jdbc.JdbcSchema;
+import org.apache.calcite.plan.RelTraitDef;
+import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.sql.SqlDialect;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.pretty.SqlPrettyWriter;
+import org.apache.calcite.tools.Frameworks;
+import org.apache.calcite.tools.Programs;
+import org.apache.calcite.tools.RelBuilder;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+/**
+ * Small utility class to help with connection to databases, building, and writing sql.
+ */
+public class CalciteHelper {
+    public static final String DEFAULT_SCHEMA = "PUBLIC";
+    private final DataSource dataSource;
+    private final String schemaName;
+    private final SqlDialect dialect;
+
+    /**
+     * Initialize the helper with a datasource and it's schema.
+     *
+     * @param dataSource  The datasource to make connections with.
+     * @param schemaName  The name of the schema where the tables are stored.
+     *
+     * @throws SQLException if failed while making a connection to the database.
+     */
+    public CalciteHelper(DataSource dataSource, String schemaName)
+            throws SQLException {
+        this.dataSource = dataSource;
+        this.schemaName = schemaName;
+        dialect = SqlDialect.create(getConnection().getMetaData());
+    }
+
+    /**
+     * Creates a {@link RelBuilder} which can be used to build sql.
+     *
+     * @return a {@link RelBuilder} or null if an error occurred.
+     */
+    public RelBuilder getNewRelBuilder() {
+        try {
+            return getBuilder(dataSource, schemaName);
+        } catch (SQLException ignored) {
+        }
+        return null;
+    }
+
+    /**
+     * Creates a connection to the database from the {@link #dataSource}.
+     *
+     * @return the connection.
+     *
+     * @throws SQLException if can't create a connection.
+     */
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    /**
+     * Creates a {@link RelToSqlConverter} which allows a {@link org.apache.calcite.rel.RelNode} to
+     * be converted into a sql query.
+     *
+     * @return the relnode to sql converter.
+     *
+     */
+    public RelToSqlConverter getNewRelToSqlConverter() {
+        return new RelToSqlConverter(dialect);
+    }
+
+    /**
+     * Creates a {@link RelBuilder} with a root scema of {@link #DEFAULT_SCHEMA}.
+     *
+     * @param dataSource  The dataSource for the jdbc schema.
+     *
+     * @return the relbuilder from Calcite.
+     *
+     * @throws SQLException if can't readSqlResultSet from database.
+     */
+    public static RelBuilder getBuilder(DataSource dataSource) throws SQLException {
+        return getBuilder(dataSource, DEFAULT_SCHEMA);
+    }
+
+
+    /**
+     * Creates a {@link RelBuilder} with the given schema.
+     *
+     * @param dataSource  The dataSource for the jdbc schema.
+     * @param schemaName  The name of the schema used for the database.
+     *
+     * @return the relbuilder from Calcite.
+     *
+     * @throws SQLException if can't readSqlResultSet from database.
+     */
+    public static RelBuilder getBuilder(DataSource dataSource, String schemaName) throws SQLException {
+        SchemaPlus rootSchema = Frameworks.createRootSchema(true);
+        return RelBuilder.create(
+                Frameworks.newConfigBuilder()
+                        .parserConfig(SqlParser.Config.DEFAULT)
+                        .defaultSchema(addSchema(rootSchema, dataSource, schemaName))
+                        .traitDefs((List<RelTraitDef>) null)
+                        .programs(Programs.heuristicJoinOrder(Programs.RULE_SET, true, 2))
+                        .build()
+        );
+    }
+
+    /**
+     * Adds the schema name to the rootSchema.
+     *
+     * @param rootSchema  The calcite schema for the database.
+     * @param dataSource  The dataSource for the jdbc schema.
+     * @param schemaName  The name of the schema used for the database.
+     *
+     * @return the schema.
+     */
+    private static SchemaPlus addSchema(SchemaPlus rootSchema, DataSource dataSource, String schemaName) {
+        return rootSchema.add(// avg tests run at ~75-100ms
+                schemaName,
+                JdbcSchema.create(rootSchema, null, dataSource, null, null)
+        );
+        // todo look into timing/behavior of cloneschema (above is faster, but it could just be a result of H2)
+        //        rootSchema.setCacheEnabled(true); //almost no effect
+        //        return rootSchema.add( // avg tests run at ~200ms
+        //                schemaName,
+        //                new CloneSchema(
+        //                        rootSchema.add(schemaName, JdbcSchema.create(rootSchema, null, dataSource, null,
+        // null))
+        //                )
+        //        );
+    }
+
+    public SqlPrettyWriter getNewSqlWriter() {
+        return new SqlPrettyWriter(dialect);
+    }
+
+    /**
+     * Gets the name of a table with it's schema (i.e. table to "schema"."table").
+     *
+     * @param sqlTableName  The name of the table to escape.
+     *
+     * @return the escaped schema/table
+     */
+    public String escapeTableName(String sqlTableName) {
+        return dialect.quoteIdentifier(schemaName) + "." + dialect.quoteIdentifier(sqlTableName);
+    }
+}
