2b698c332497f816d2bb998c4428baca0b7a4470
Monica Pardeshi <monica.pardeshi@gmail.com>
2017-07-18 22:40:49 -0500

    edited entrypoint in Dockerfile and moved those commands to entrypoint.sh

---
+++ fili-wikipedia-example/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh 
+
+export HOSTIP="$(resolveip -s $HOSTNAME)" 
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
+echo "Waiting for Druid to finish setting up"
+while ! curl http://localhost:3001/druid/coordinator/v1/datasources | grep -q "wikipedia"; do
+      sleep 5
+done
+echo "Druid finished setting up. Starting Fili"
+mvn -pl fili-generic-example exec:java -Dbard__fili_port=9998 
+    -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 
+    -Dbard__non_ui_druid_broker=http://localhost:8082/druid/v2 
+    -Dbard__ui_druid_broker=http://localhost:8082/druid/v2
