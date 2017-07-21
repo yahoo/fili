058b525ce392ab849912b984d2d501ba6cafad38
Monica Pardeshi <monica.pardeshi@gmail.com>
2017-07-21 11:43:45 -0500

    made sure that Docker doesn't run forever if Druid fails to start

--- fili-wikipedia-example/entrypoint.sh
+++ fili-wikipedia-example/entrypoint.sh
@@ -3,9 +3,19 @@
 export HOSTIP="$(resolveip -s $HOSTNAME)" 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf & 
 echo "Waiting for Druid to finish setting up"
+
+time_left=300
+
 while ! curl http://localhost:8081/druid/coordinator/v1/datasources | grep -q "wikipedia"; do
-      sleep 5
+    if [ "$time_left" -le 0 ]
+    then
+        echo "Druid is having trouble setting up"
+        exit 
+    fi
+    sleep 5
+    time_left=$(( time_left - 5 ))
 done
+
 echo "Druid finished setting up. Starting Fili"
 mvn -pl fili-generic-example exec:java -Dbard__fili_port=9998 
     -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 
