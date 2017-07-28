b91ea28ee7acb046e8366e9232a4db69683a13b9
Kevin Hinterlong <kevinhinterlong@users.noreply.github.com>
2017-07-28 11:16:49 -0500

    Tell maven to use more memory during builds (#452)
    * Tell maven to use more memory during builds
    * Use trusty builds

--- travis/deploy.bash
+++ travis/deploy.bash
@@ -2,6 +2,8 @@
 PUBLISH_WHITELIST="master"
 RELEASE_TAG_REGEX="^[0-9]+\\.[0-9]+\\.[0-9]+"
 
+export MAVEN_OPTS="-Xmx3000m"
+
 # Deploy if this has a travis tag and is on an approved branch
 MATCHING_TAG=$(echo $TRAVIS_TAG | egrep ${RELEASE_TAG_REGEX})
 if [[ ${MATCHING_TAG} != "" ]]; then
