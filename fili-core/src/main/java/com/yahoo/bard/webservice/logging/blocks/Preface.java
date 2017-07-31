22719591f3f667cfc51a1e8edc6952ec39f0a202
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-07-31 11:15:49 -0500

    Lazily find username for preface logblock

--- fili-core/src/main/java/com/yahoo/bard/webservice/logging/blocks/Preface.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/logging/blocks/Preface.java
@@ -5,12 +5,15 @@ package com.yahoo.bard.webservice.logging.blocks;
 import com.yahoo.bard.webservice.logging.LogInfo;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.Principal;
 import java.util.Collections;
+import java.util.function.Supplier;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
@@ -27,8 +30,20 @@ public class Preface implements LogInfo {
 
     protected final String uri;
     protected final String method;
-    protected final String user;
     protected final MultivaluedMap<String, String> headers;
+
+    @JsonIgnore
+    protected final Supplier<String> user;
+
+    /**
+     * Calls get on the {@link #user} at serialization.
+     *
+     * @return the name of the user
+     */
+    @JsonProperty
+    public String getUser() {
+        return user.get();
+    }
 
     /**
      * Constructor.
@@ -38,14 +53,17 @@ public class Preface implements LogInfo {
     public Preface(ContainerRequestContext request) {
         uri = request.getUriInfo().getRequestUri().toASCIIString();
         method = request.getMethod();
-        Principal principal = request.getSecurityContext().getUserPrincipal();
-        if (principal != null) {
-            user = principal.getName();
-        } else {
-            // No principal found
-            LOG.warn("No user principal detected when building the Preface. This shouldn't happen!");
-            user = NO_USER_PRINCIPAL_USER_NAME;
-        }
+
+        user = () -> {
+            Principal principal = request.getSecurityContext().getUserPrincipal();
+            if (principal != null) {
+                return principal.getName();
+            } else {
+                // No principal found
+                LOG.warn("No user principal detected when building the Preface. This shouldn't happen!");
+                return NO_USER_PRINCIPAL_USER_NAME;
+            }
+        };
 
         headers = request.getHeaders();
         //Discard all cookies from header by overriding the entry
--- fili-core/src/test/groovy/com/yahoo/bard/webservice/logging/blocks/PrefaceSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/logging/blocks/PrefaceSpec.groovy
@@ -2,16 +2,21 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.logging.blocks
 
+import com.yahoo.bard.webservice.application.ObjectMappersSuite
+
 import spock.lang.Specification
 
 import java.nio.file.attribute.UserPrincipal
+import java.security.Principal
 
+import javax.security.auth.Subject
 import javax.ws.rs.container.ContainerRequestContext
-import javax.ws.rs.core.MultivaluedMap
+import javax.ws.rs.core.MultivaluedHashMap
 import javax.ws.rs.core.SecurityContext
 import javax.ws.rs.core.UriInfo
 
 class PrefaceSpec extends Specification {
+    private static final MAPPER = new ObjectMappersSuite().getMapper()
 
     ContainerRequestContext mockRequest = Mock(ContainerRequestContext) {
         // Implement things we don't care about but Preface needs
@@ -19,7 +24,7 @@ class PrefaceSpec extends Specification {
             getRequestUri() >> "http://example.com".toURI()
         }
         getMethod() >> ""
-        getHeaders() >> Mock(MultivaluedMap)
+        getHeaders() >> new MultivaluedHashMap()
     }
 
     def "User set to default value if userPrincipal is null"() {
@@ -29,7 +34,9 @@ class PrefaceSpec extends Specification {
         }
 
         expect: "Making a Preface with it sets the user to the default value"
-        new Preface(mockRequest).user == Preface.NO_USER_PRINCIPAL_USER_NAME
+        def preface = new Preface(mockRequest)
+        preface.getUser() == Preface.NO_USER_PRINCIPAL_USER_NAME
+        MAPPER.valueToTree(preface).toString() == """{"uri":"http://example.com","method":"","headers":{"Cookie":["Cookies not present in header"]},"user":"NO_USER_PRINCIPAL"}"""
     }
 
     def "User set to username value if userPrincipal is not null"() {
@@ -42,6 +49,65 @@ class PrefaceSpec extends Specification {
         }
 
         expect: "Making a Preface with it sets the user to the user name"
-        new Preface(mockRequest).user == knownUserName
+        def preface = new Preface(mockRequest)
+        preface.getUser() == knownUserName
+        MAPPER.valueToTree(preface).toString() == """{"uri":"http://example.com","method":"","headers":{"Cookie":["Cookies not present in header"]},"user":"${knownUserName}"}"""
+    }
+
+    def "User set to username value at serialization time"() {
+        given: "A request with a userPrincipal"
+        def securityContext = new MockSecurityContext(null)
+        mockRequest.getSecurityContext() >> securityContext
+        def preface = new Preface(mockRequest)
+
+        expect: "Use default username value"
+        preface.getUser() == Preface.NO_USER_PRINCIPAL_USER_NAME
+
+        when: "Set the username value"
+        String knownUserName = "Known User Name"
+        def userPrincipal = new MockUserPrincipal(knownUserName)
+        securityContext.setUserPrincipal(userPrincipal)
+
+        then: "Use the set username value"
+        preface.getUser() == knownUserName
+    }
+
+    static class MockUserPrincipal implements UserPrincipal {
+        private final String name
+
+        MockUserPrincipal(String name) {
+            this.name = name
+        }
+
+        @Override
+        String getName() {
+            return name
+        }
+
+        @Override
+        boolean implies(Subject subject) { return false }
+    }
+
+    static class MockSecurityContext implements SecurityContext {
+        private UserPrincipal userPrincipal
+        MockSecurityContext(UserPrincipal userPrincipal) {
+            this.userPrincipal = userPrincipal;
+        }
+
+        void setUserPrincipal(UserPrincipal userPrincipal) {
+            this.userPrincipal = userPrincipal;
+        }
+
+        @Override
+        Principal getUserPrincipal() {
+            return userPrincipal
+        }
+
+        @Override
+        boolean isUserInRole(String role) { return false }
+        @Override
+        boolean isSecure() { return false }
+        @Override
+        String getAuthenticationScheme() { return null }
     }
 }
