b42089e98f7cd612ad92dd53b4a544218b9ae87f
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-08-02 14:46:50 -0500

    Add aggregation, time converters, and api to field mapper

---
+++ fili-sql/src/main/java/com/yahoo/bard/webservice/sql/aggregation/DefaultDruidSqlAggregationConverter.java
@@ -0,0 +1,31 @@
+// Copyright 2017 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.sql.aggregation;
+
+import com.yahoo.bard.webservice.druid.model.aggregation.Aggregation;
+import com.yahoo.bard.webservice.sql.ApiToFieldMapper;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * The default implementation of sql aggregations based on druid aggregations.
+ */
+public class DefaultDruidSqlAggregationConverter implements DruidSqlAggregationConverter {
+    /**
+     * Constructor.
+     */
+    public DefaultDruidSqlAggregationConverter() {
+
+    }
+
+    @Override
+    public Optional<SqlAggregationBuilder> fromDruidType(Aggregation aggregation, ApiToFieldMapper apiToFieldMapper) {
+        String aggregationType = aggregation.getType().toLowerCase(Locale.ENGLISH);
+        return Arrays.stream(DefaultSqlAggregationType.values())
+                .filter(sqlAggregationType -> aggregationType.contains(sqlAggregationType.getType()))
+                .map(sqlAggregationType -> sqlAggregationType.with(aggregation, apiToFieldMapper))
+                .findFirst();
+    }
+}
