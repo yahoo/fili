1f1f641d1706268421c2c9890e9cbb700e5f7d5f
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-08-15 15:40:21 -0500

    Groovyify tests

--- fili-sql/src/test/groovy/com/yahoo/bard/webservice/sql/DruidQueryToSqlConverterSpec.groovy
+++ fili-sql/src/test/groovy/com/yahoo/bard/webservice/sql/DruidQueryToSqlConverterSpec.groovy
@@ -3,7 +3,11 @@ package com.yahoo.bard.webservice.sql
 import static com.yahoo.bard.webservice.data.time.DefaultTimeGrain.DAY
 import static com.yahoo.bard.webservice.data.time.DefaultTimeGrain.MONTH
 import static com.yahoo.bard.webservice.data.time.DefaultTimeGrain.YEAR
-import static com.yahoo.bard.webservice.database.Database.SCHEMA
+import static com.yahoo.bard.webservice.database.Database.ADDED
+import static com.yahoo.bard.webservice.database.Database.DELETED
+import static com.yahoo.bard.webservice.database.Database.IS_ROBOT
+import static com.yahoo.bard.webservice.database.Database.METRO_CODE
+import static com.yahoo.bard.webservice.database.Database.TIME
 import static com.yahoo.bard.webservice.druid.model.orderby.SortDirection.ASC
 import static com.yahoo.bard.webservice.druid.model.orderby.SortDirection.DESC
 import static com.yahoo.bard.webservice.druid.model.query.AllGranularity.INSTANCE
@@ -13,12 +17,8 @@ import static com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder.END
 import static com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder.START
 import static com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder.getDimensions
 import static com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder.getWikitickerDatasource
-import static com.yahoo.bard.webservice.database.Database.ADDED
-import static com.yahoo.bard.webservice.database.Database.DELETED
-import static com.yahoo.bard.webservice.database.Database.IS_ROBOT
-import static com.yahoo.bard.webservice.database.Database.METRO_CODE
-import static java.util.Arrays.asList
 
+import com.yahoo.bard.webservice.database.Database
 import com.yahoo.bard.webservice.druid.model.orderby.LimitSpec
 import com.yahoo.bard.webservice.druid.model.orderby.OrderByColumn
 import com.yahoo.bard.webservice.druid.model.orderby.SortDirection
@@ -26,7 +26,6 @@ import com.yahoo.bard.webservice.druid.model.query.DruidQuery
 import com.yahoo.bard.webservice.druid.model.query.Granularity
 import com.yahoo.bard.webservice.druid.model.query.GroupByQuery
 import com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder
-import com.yahoo.bard.webservice.database.Database
 import com.yahoo.bard.webservice.sql.helper.CalciteHelper
 
 import spock.lang.Specification
@@ -35,7 +34,8 @@ import spock.lang.Unroll
 class DruidQueryToSqlConverterSpec extends Specification {
     static CalciteHelper calciteHelper = new CalciteHelper(Database.getDataSource())
     static DruidQueryToSqlConverter druidQueryToSqlConverter = new DruidQueryToSqlConverter(calciteHelper)
-    static ApiToFieldMapper apiToFieldMapper = SimpleDruidQueryBuilder.getApiToFieldMapper("api_", "")
+    static ApiToFieldMapper apiToFieldMapper = SimpleDruidQueryBuilder.getApiToFieldMapper(API_PREPEND, "")
+    static private final String API_PREPEND = "api_"
 
     private static GroupByQuery getGroupByQuery(
             Granularity timeGrain,
@@ -43,20 +43,20 @@ class DruidQueryToSqlConverterSpec extends Specification {
             LimitSpec limitSpec
     ) {
         return new GroupByQuery(
-                getWikitickerDatasource("api_", ""),
+                getWikitickerDatasource(API_PREPEND, ""),
                 timeGrain,
-                getDimensions(dimensions.collect { "api_" + it }),
+                getDimensions(dimensions.collect { API_PREPEND + it }),
                 null,
                 null,
-                asList(sum("api_" + ADDED), sum("api_" + DELETED)),
-                asList(),
-                asList(interval(START, END)),
+                [sum(API_PREPEND + ADDED), sum(API_PREPEND + DELETED)],
+                [],
+                [interval(START, END)],
                 limitSpec
         )
     }
 
     private static LimitSpec getSort(List<String> columns, List<SortDirection> sortDirections) {
-        LinkedHashSet<OrderByColumn> sorts = new LinkedHashSet<>()
+        LinkedHashSet<OrderByColumn> sorts = []
         for (int i = 0; i < columns.size(); i++) {
             sorts.add(
                     new OrderByColumn(columns.get(i), sortDirections.get(i))
@@ -73,15 +73,15 @@ class DruidQueryToSqlConverterSpec extends Specification {
         def sql = druidQueryToSqlConverter.buildSqlQuery(query, apiToFieldMapper)
 
         expect:
-        sql.endsWith(expectedOutput)
+        sql.endsWith(expectedOutput.trim())
 
         where:
-        grain    | dims                         | metrics                | metricDirections  | expectedOutput
-        DAY      | asList(METRO_CODE)           | asList(ADDED)          | asList(DESC)      | 'ORDER BY YEAR("TIME"), DAYOFYEAR("TIME"), SUM("added") DESC NULLS FIRST, "metroCode"'
-        DAY      | asList(METRO_CODE, IS_ROBOT) | asList(ADDED)          | asList(DESC)      | 'ORDER BY YEAR("TIME"), DAYOFYEAR("TIME"), SUM("added") DESC NULLS FIRST, "metroCode", "isRobot"'
-        DAY      | asList()                     | asList(ADDED, DELETED) | asList(DESC, ASC) | 'ORDER BY YEAR("TIME"), DAYOFYEAR("TIME"), SUM("added") DESC NULLS FIRST, SUM("deleted")'
-        YEAR     | asList(METRO_CODE)           | asList()               | asList()          | 'ORDER BY YEAR("TIME"), "metroCode"'
-        MONTH    | asList()                     | asList()               | asList()          | 'ORDER BY YEAR("TIME"), MONTH("TIME")'
-        INSTANCE | asList()                     | asList()               | asList()          | 'ORDER BY "TIME"'
+        grain    | dims                   | metrics          | metricDirections | expectedOutput
+        DAY      | [METRO_CODE]           | [ADDED]          | [DESC]           | """ ORDER BY YEAR("${TIME}"), DAYOFYEAR("${TIME}"), SUM("${ADDED}") DESC NULLS FIRST, "${METRO_CODE}" """
+        DAY      | [METRO_CODE, IS_ROBOT] | [ADDED]          | [DESC]           | """ ORDER BY YEAR("${TIME}"), DAYOFYEAR("${TIME}"), SUM("${ADDED}") DESC NULLS FIRST, "${METRO_CODE}", "${IS_ROBOT}" """
+        DAY      | []                     | [ADDED, DELETED] | [DESC, ASC]      | """ ORDER BY YEAR("${TIME}"), DAYOFYEAR("${TIME}"), SUM("${ADDED}") DESC NULLS FIRST, SUM("${DELETED}") """
+        YEAR     | [METRO_CODE]           | []               | []               | """ ORDER BY YEAR("${TIME}"), "${METRO_CODE}" """
+        MONTH    | []                     | []               | []               | """ ORDER BY YEAR("${TIME}"), MONTH("${TIME}") """
+        INSTANCE | []                     | []               | []               | """ ORDER BY "${TIME}" """
     }
 }
