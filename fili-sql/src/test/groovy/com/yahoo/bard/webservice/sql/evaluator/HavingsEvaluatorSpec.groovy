6265d8dce2bd6b96eda27a24ce508be9365869ab
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-08-15 15:09:59 -0500

    Groovyify the tests

--- fili-sql/src/test/groovy/com/yahoo/bard/webservice/sql/evaluator/HavingsEvaluatorSpec.groovy
+++ fili-sql/src/test/groovy/com/yahoo/bard/webservice/sql/evaluator/HavingsEvaluatorSpec.groovy
@@ -2,7 +2,10 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.sql.evaluator
 
+import static com.yahoo.bard.webservice.database.Database.ADDED
+import static com.yahoo.bard.webservice.database.Database.DELETED
 import static com.yahoo.bard.webservice.database.Database.SCHEMA
+import static com.yahoo.bard.webservice.database.Database.WIKITICKER
 import static com.yahoo.bard.webservice.sql.builders.Aggregator.max
 import static com.yahoo.bard.webservice.sql.builders.Aggregator.min
 import static com.yahoo.bard.webservice.sql.builders.Aggregator.sum
@@ -11,16 +14,12 @@ import static com.yahoo.bard.webservice.sql.builders.Havings.equals
 import static com.yahoo.bard.webservice.sql.builders.Havings.gt
 import static com.yahoo.bard.webservice.sql.builders.Havings.lt
 import static com.yahoo.bard.webservice.sql.builders.Havings.or
-import static com.yahoo.bard.webservice.database.Database.ADDED
-import static com.yahoo.bard.webservice.database.Database.DELETED
-import static com.yahoo.bard.webservice.database.Database.WIKITICKER
-import static java.util.Arrays.asList
 
 import com.yahoo.bard.webservice.data.time.DefaultTimeGrain
+import com.yahoo.bard.webservice.database.Database
 import com.yahoo.bard.webservice.sql.ApiToFieldMapper
 import com.yahoo.bard.webservice.sql.aggregation.DefaultDruidSqlAggregationConverter
 import com.yahoo.bard.webservice.sql.builders.SimpleDruidQueryBuilder
-import com.yahoo.bard.webservice.database.Database
 import com.yahoo.bard.webservice.sql.helper.CalciteHelper
 import com.yahoo.bard.webservice.sql.helper.DefaultSqlTimeConverter
 
@@ -80,12 +79,12 @@ class HavingsEvaluatorSpec extends Specification {
                 toString();
         sql.contains(expectedHavingSql)
         where: "we have"
-        having                                                | aggregations                                 | expectedHavingSql
-        gt(API + ADDED, ONE)                                  | asList(sum(API + ADDED))                     | "HAVING SUM(`${ADDED}`) > 1"
-        lt(API + DELETED, ONE)                                | asList(sum(API + DELETED))                   | "HAVING SUM(`${DELETED}`) < 1"
-        equals(API + DELETED, ONE)                            | asList(sum(API + DELETED), sum(API + ADDED)) | "HAVING SUM(`${DELETED}`) = 1"
-        or(equals(API + DELETED, ONE), lt(API + ADDED, TWO))  | asList(max(API + DELETED), min(API + ADDED)) | "HAVING MAX(`${DELETED}`) = 1 OR MIN(`${ADDED}`) < 2"
-        and(equals(API + DELETED, ONE), lt(API + ADDED, TWO)) | asList(max(API + DELETED), min(API + ADDED)) | "HAVING MAX(`${DELETED}`) = 1 AND MIN(`${ADDED}`) < 2"
+        having                                                | aggregations                           | expectedHavingSql
+        gt(API + ADDED, ONE)                                  | [sum(API + ADDED)]                     | "HAVING SUM(`${ADDED}`) > 1"
+        lt(API + DELETED, ONE)                                | [sum(API + DELETED)]                   | "HAVING SUM(`${DELETED}`) < 1"
+        equals(API + DELETED, ONE)                            | [sum(API + DELETED), sum(API + ADDED)] | "HAVING SUM(`${DELETED}`) = 1"
+        or(equals(API + DELETED, ONE), lt(API + ADDED, TWO))  | [max(API + DELETED), min(API + ADDED)] | "HAVING MAX(`${DELETED}`) = 1 OR MIN(`${ADDED}`) < 2"
+        and(equals(API + DELETED, ONE), lt(API + ADDED, TWO)) | [max(API + DELETED), min(API + ADDED)] | "HAVING MAX(`${DELETED}`) = 1 AND MIN(`${ADDED}`) < 2"
 
     }
 
