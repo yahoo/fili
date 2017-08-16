7425565ce8f60a097b32e0f23af37c5610fce687
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-08-16 12:48:31 -0500

    Address comments regarding aggregations

--- fili-sql/src/main/java/com/yahoo/bard/webservice/sql/aggregation/DefaultDruidSqlAggregationConverter.java
+++ fili-sql/src/main/java/com/yahoo/bard/webservice/sql/aggregation/DefaultDruidSqlAggregationConverter.java
@@ -6,26 +6,65 @@ import com.yahoo.bard.webservice.druid.model.aggregation.Aggregation;
 import com.yahoo.bard.webservice.sql.ApiToFieldMapper;
 
 import java.util.Arrays;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 /**
- * The default implementation of sql aggregations based on druid aggregations.
+ * The default implementation of  mapping from Druid's {@link Aggregation} to a {@link SqlAggregation}.
  */
-public class DefaultDruidSqlAggregationConverter implements DruidSqlAggregationConverter {
+public class DefaultDruidSqlAggregationConverter
+        implements BiFunction<Aggregation, ApiToFieldMapper, Optional<SqlAggregation>> {
+    private Map<String, SqlAggregationType> druidToSqlAggregation;
+
     /**
-     * Constructor.
+     * Constructors a map fron druid to sql aggregations using {@link #getDefaultDruidToSqlAggregation()}.
      */
     public DefaultDruidSqlAggregationConverter() {
-
+        this(getDefaultDruidToSqlAggregation());
     }
 
+    /**
+     * Use the given map for converting from druid to sql aggregations.
+     *
+     * @param druidToSqlAggregation  The mapping from Druid aggregation name to Sql aggregation type.
+     */
+    public DefaultDruidSqlAggregationConverter(Map<String, SqlAggregationType> druidToSqlAggregation) {
+        this.druidToSqlAggregation = druidToSqlAggregation;
+    }
+
+    /**
+     * The default mapping from druid to sql aggregations defined by {@link DefaultSqlAggregationType}.
+     *
+     * @return the mapping from druid aggregation names to sql aggregation types.
+     */
+    public static Map<String, SqlAggregationType> getDefaultDruidToSqlAggregation() {
+        Map<String, SqlAggregationType> druidToSqlAggregation = new HashMap<>();
+        Arrays.stream(DefaultSqlAggregationType.values())
+                .forEach(defaultSqlAggregationType -> {
+                    defaultSqlAggregationType.getSupportedDruidAggregations()
+                            .forEach(druidAggregation -> {
+                                druidToSqlAggregation.put(druidAggregation, defaultSqlAggregationType);
+                            });
+                });
+        return druidToSqlAggregation;
+    }
+
+    /**
+     * Finds the corresponding {@link SqlAggregation} from a druid aggregation.
+     *
+     * @param aggregation  The druid aggregation, i.e.
+     * {@link com.yahoo.bard.webservice.druid.model.aggregation.LongSumAggregation}.
+     * @param apiToFieldMapper  the mapping from api name to field name of the aggregation.
+     *
+     *  @return the supported sql aggregation type.
+     */
     @Override
-    public Optional<SqlAggregationBuilder> fromDruidType(Aggregation aggregation, ApiToFieldMapper apiToFieldMapper) {
-        String aggregationType = aggregation.getType().toLowerCase(Locale.ENGLISH);
-        return Arrays.stream(DefaultSqlAggregationType.values())
-                .filter(sqlAggregationType -> aggregationType.contains(sqlAggregationType.getType()))
-                .map(sqlAggregationType -> sqlAggregationType.with(aggregation, apiToFieldMapper))
-                .findFirst();
+    public Optional<SqlAggregation> apply(Aggregation aggregation, ApiToFieldMapper apiToFieldMapper) {
+        String aggregationType = aggregation.getType();
+        return druidToSqlAggregation.containsKey(aggregationType) ?
+                Optional.of(druidToSqlAggregation.get(aggregationType).getSqlAggregation(aggregation, apiToFieldMapper))
+                : Optional.empty();
     }
 }
---
+++ fili-sql/src/main/java/com/yahoo/bard/webservice/sql/aggregation/SqlAggregation.java
@@ -0,0 +1,49 @@
+// Copyright 2017 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.sql.aggregation;
+
+import org.apache.calcite.sql.SqlAggFunction;
+
+/**
+ * A sql aggregation value object containing all the necessary information to build a sql aggregation.
+ */
+public class SqlAggregation {
+    private final String fieldName;
+    private final SqlAggFunction sqlAggFunction;
+
+    /**
+     * Constructor.
+     *
+     * @param fieldName  The name of the field being aggregated on.
+     * @param sqlAggFunction  The sql aggregation to be done on the field.
+     */
+    public SqlAggregation(String fieldName, SqlAggFunction sqlAggFunction) {
+        this.fieldName = fieldName;
+        this.sqlAggFunction = sqlAggFunction;
+    }
+
+    /**
+     * Gets the alias name to be used for the aggregation (i.e. the AS "fieldName") part of a sql aggregation.
+     * @return
+     */
+    public String getSqlAggregationAsName() {
+        return getSqlAggregationFieldName();
+    }
+
+    /**
+     * The field name to call the aggregation on (i.e. the SUM("fieldName")) part of a sql aggregation.
+     * @return
+     */
+    public String getSqlAggregationFieldName() {
+        return fieldName;
+    }
+
+    /**
+     * Gets the sql aggregation to be used on the field.
+     *
+     * @return the sql aggregation function.
+     */
+    public SqlAggFunction getSqlAggFunction() {
+        return sqlAggFunction;
+    }
+}
