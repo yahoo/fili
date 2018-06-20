03e58b3b0f5c2ba262e27b5eb6000577ecfb4de2
ForestCold <hanfei.lin@hotmail.com>
2018-06-20 14:53:22 -0500

    Change Template from abstract class to class

--- wiki-test/src/main/java/com/yahoo/wiki/webservice/data/config/Template.java
+++ wiki-test/src/main/java/com/yahoo/wiki/webservice/data/config/Template.java
@@ -5,10 +5,6 @@ package com.yahoo.wiki.webservice.data.config;
 /**
  * Abstract Template class.
  */
-public abstract class Template {
+public class Template {
 
-    /**
-     * Abstract Constructor.
-     */
-    protected Template() { }
 }
