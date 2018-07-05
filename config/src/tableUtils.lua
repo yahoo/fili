664faac098f89fdfa8316e4665f5a1edb5a8f073
ForestCold <hanfei.lin@hotmail.com>
2018-07-05 17:24:11 -0500

    Add lua config for tables

---
+++ config/src/tableUtils.lua
@@ -0,0 +1,37 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ylin08
+-- Date: 7/5/18
+-- Time: 5:06 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+local M = {}
+
+function M.add_phy_tables(tables, t)
+    for name, physical_table in pairs(tables) do
+        table.insert(t, {
+            name = name,
+            description =physical_table[1] or physical_table.description,
+            metrics = physical_table[2] or physical_table.metrics,
+            dimensions = physical_table[3] or physical_table.dimensions,
+            granularity = physical_table[4] or physical_table.granularity
+        })
+    end
+end
+
+function M.add_log_tables(tables, t)
+    for name, logical_table in pairs(tables) do
+        table.insert(t, {
+            name = name,
+            description = logical_table[1] or logical_table.description,
+            metrics = logical_table[2] or logical_table.metrics,
+            dimensions = logical_table[3] or logical_table.dimensions,
+            granularity = logical_table[4] or logical_table.granularity,
+            physicalTables = logical_table[5] or logical_table.physical_tables
+        })
+    end
+end
+
+return M
+
---
+++ config/src/tables.lua
@@ -0,0 +1,62 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ylin08
+-- Date: 7/5/18
+-- Time: 4:52 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+local utils = require("src/tableUtils")
+local parser = require("src/jsonParser")
+
+local M = {
+    physicalTables = {},
+    logicalTables = {}
+}
+
+DEFAULT = {
+    ALL_DIM = {
+        "COMMENT",
+        "COUNTRY_ISO_CODE",
+        "REGION_ISO_CODE",
+        "PAGE",
+        "USER",
+        "IS_UNPATROLLED",
+        "IS_NEW",
+        "IS_ROBOT",
+        "IS_ANONYMOUS",
+        "IS_MINOR",
+        "NAMESPACE",
+        "CHANNEL",
+        "COUNTRY_NAME",
+        "REGION_NAME",
+        "METRO_CODE",
+        "CITY_NAME"
+    },
+    ALL_METRICS ={
+        "count",
+        "added",
+        "delta",
+        "deleted",
+        "averageAddedPerHour",
+        "averageDeletedPerHour",
+        "plusAvgAddedDeleted",
+        "MinusAddedDelta",
+        "cardOnPage",
+        "bigThetaSketch"
+    }
+}
+
+PHYSICALTABLES = {
+    wikiticker = {nil, {"added", "delta", "deleted"}, DEFAULT.ALL_DIM, "HOUR"}
+}
+
+LOGICALTABLES = {
+    WIKIPEDIA = {nil, {"count", "added", "delta", "deleted"}, DEFAULT.ALL_DIM, {"ALL", "HOUR", "DAY"}, {"wikiticker"}},
+    logicalTableTesterOne = {nil, DEFAULT.ALL_METRICS, DEFAULT.ALL_DIM, {"ALL", "HOUR", "DAY"}, {"wikiticker"}}
+}
+
+utils.add_phy_tables(PHYSICALTABLES, M.physicalTables)
+utils.add_log_tables(LOGICALTABLES, M.logicalTables)
+
+parser.save("TableConfigTemplate.json", M)
