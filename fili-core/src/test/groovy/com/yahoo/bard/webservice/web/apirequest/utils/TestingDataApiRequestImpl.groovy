e6ad6aa5e28f2669bf18ddae324a928d8a62a7a4
Michael McLawhorn <mclawhor@yahoo-inc.com>
2018-07-24 18:45:57 -0500

    Add DefaultDruidFilterBuilder

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/web/apirequest/utils/TestingDataApiRequestImpl.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/web/apirequest/utils/TestingDataApiRequestImpl.groovy
@@ -2,6 +2,7 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.web.apirequest.utils
 
+import com.yahoo.bard.webservice.data.filterbuilders.DefaultDruidFilterBuilder
 import com.yahoo.bard.webservice.data.time.DefaultTimeGrain
 import com.yahoo.bard.webservice.druid.model.having.Having
 import com.yahoo.bard.webservice.table.LogicalTable
@@ -31,7 +32,7 @@ class TestingDataApiRequestImpl extends DataApiRequestImpl {
                 0,  // topN
                 Long.MAX_VALUE, // asynchAfter
                 (DateTimeZone) null,
-                null, // DateTimeSort
+                new DefaultDruidFilterBuilder(),
                 null,
                 Optional.empty()
         )
