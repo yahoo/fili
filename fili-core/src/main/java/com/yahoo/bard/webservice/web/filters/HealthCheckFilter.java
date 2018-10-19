87187c73813bd6519d1ae3208b835319374609b5
Benjamin Efron <befron@oath.com>
2018-10-19 17:37:21 -0500

    added logging to healthcheck filter

--- fili-core/src/main/java/com/yahoo/bard/webservice/web/filters/HealthCheckFilter.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/web/filters/HealthCheckFilter.java
@@ -2,6 +2,7 @@
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 package com.yahoo.bard.webservice.web.filters;
 
+import com.yahoo.bard.webservice.application.AbstractBinderFactory;
 import com.yahoo.bard.webservice.application.HealthCheckRegistryFactory;
 import com.yahoo.bard.webservice.logging.RequestLog;
 
@@ -11,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -40,6 +42,15 @@ public class HealthCheckFilter implements ContainerRequestFilter {
             // See if we have any unhealthy checks
             Map<String, Result> unhealthyChecks = getUnhealthy();
             if (!unhealthyChecks.keySet().isEmpty()) {
+                if (unhealthyChecks.keySet().contains(AbstractBinderFactory.HEALTH_CHECK_NAME_LOOKUP_METADATA)) {
+                    StringBuilder debugMsgBuilder = new StringBuilder();
+                    if (requestContext.getSecurityContext().getUserPrincipal() != null) {
+                        String user = requestContext.getSecurityContext().getUserPrincipal().getName();
+                        debugMsgBuilder.append("User=").append(user).append("\t");
+                    }
+                    debugMsgBuilder.append(renderUri(requestContext.getUriInfo().getRequestUri())).append("\t");
+                }
+
                 unhealthyChecks.entrySet()
                         .forEach(entry -> LOG.error("Healthcheck '{}' failed: {}", entry.getKey(), entry.getValue()));
                 RequestLog.stopTiming(this);
@@ -63,5 +74,9 @@ public class HealthCheckFilter implements ContainerRequestFilter {
         return HealthCheckRegistryFactory.getRegistry().runHealthChecks().entrySet().stream()
                 .filter(entry -> !entry.getValue().isHealthy())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    protected String renderUri(URI uri) {
+        return uri.toASCIIString();
     }
 }
