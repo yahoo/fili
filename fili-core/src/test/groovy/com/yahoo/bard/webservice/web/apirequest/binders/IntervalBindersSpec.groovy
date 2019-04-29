41088fbd4b3442e22203f35bdd50034396b21852
Will Lauer <wlauer@verizonmedia.com>
2019-04-29 11:29:59 -0500

    Fixing build errors detected by eclipse

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/web/apirequest/binders/IntervalBindersSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/web/apirequest/binders/IntervalBindersSpec.groovy
@@ -1,6 +1,6 @@
 // Copyright 2019 Yahoo Inc.
 // Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
-package groovy.com.yahoo.bard.webservice.web.apirequest.binders
+package com.yahoo.bard.webservice.web.apirequest.binders
 
 import com.yahoo.bard.webservice.config.SystemConfig
 import com.yahoo.bard.webservice.config.SystemConfigProvider
