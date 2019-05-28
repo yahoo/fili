f97bb2c0928ccc1f9e284dd5b289a7c8b49b9a07
Michael McLawhorn <mclawhor@verizonmedia.com>
2019-05-28 18:33:29 -0500

    Java skeleton

---
+++ luthier/src/main/java/com/yahoo/bard/webservice/application/LuthierBinderFactory.java
@@ -0,0 +1,72 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.application;
+
+import com.yahoo.bard.webservice.config.luthier.Factory;
+import com.yahoo.bard.webservice.config.luthier.LuthierIndustrialPark;
+import com.yahoo.bard.webservice.data.config.ConfigurationLoader;
+import com.yahoo.bard.webservice.data.config.LuthierResourceDictionaries;
+import com.yahoo.bard.webservice.data.config.dimension.DimensionConfig;
+import com.yahoo.bard.webservice.data.config.metric.MetricLoader;
+import com.yahoo.bard.webservice.data.config.metric.makers.MetricMaker;
+import com.yahoo.bard.webservice.data.config.table.TableLoader;
+import com.yahoo.bard.webservice.data.dimension.Dimension;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+/**
+ * Extend the abstract binder factory to add external configuration.
+ */
+public class LuthierBinderFactory extends AbstractBinderFactory {
+
+    @Override
+    protected ConfigurationLoader getConfigurationLoader() {
+        LuthierResourceDictionaries resourceDictionaries = new LuthierResourceDictionaries();
+        initializeDictionaries(resourceDictionaries);
+        return new LuthierIndustrialPark(resourceDictionaries, getDimensionFactories());
+    }
+
+    /**
+     * Extension point to add default dimension factories.
+     *
+     * @return  Initializing dimension factories.
+     */
+    protected Map<String, Factory<Dimension>> getDimensionFactories() {
+        return Collections.EMPTY_MAP;
+    }
+
+    /**
+     * Extension point to add default maker factories.
+     *
+     * @return  Initializing dimension factories.
+     */
+    protected Map<String, Factory<MetricMaker>> getMakerFactories() {
+        return Collections.EMPTY_MAP;
+    }
+
+    /**
+     * Extension point to initialize resources into the resource dictionaries.
+     *
+     * @param resourceDictionaries  The resource dictionaries to prefill.
+     */
+    protected void initializeDictionaries(LuthierResourceDictionaries resourceDictionaries) {
+        resourceDictionaries.getMetricMakerDictionary().putAll(LuthierResourceDictionaries.defaultMakerDictionary());
+    }
+
+    @Override
+    protected MetricLoader getMetricLoader() {
+        return null;
+    }
+
+    @Override
+    protected LinkedHashSet<DimensionConfig> getDimensionConfigurations() {
+        return null;
+    }
+
+    @Override
+    protected TableLoader getTableLoader() {
+        return null;
+    }
+}
---
+++ luthier/src/main/java/com/yahoo/bard/webservice/config/luthier/LuthierIndustrialPark.java
@@ -0,0 +1,101 @@
+// Copyright 2019 Oath Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.config.luthier;
+
+import com.yahoo.bard.webservice.data.config.ConfigurationLoader;
+import com.yahoo.bard.webservice.data.config.ResourceDictionaries;
+import com.yahoo.bard.webservice.data.dimension.Dimension;
+import com.yahoo.bard.webservice.data.dimension.DimensionDictionary;
+import com.yahoo.bard.webservice.data.metric.MetricDictionary;
+import com.yahoo.bard.webservice.table.LogicalTableDictionary;
+import com.yahoo.bard.webservice.table.PhysicalTableDictionary;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Dependency Injection container for Config Objects.
+ */
+public class LuthierIndustrialPark implements ConfigurationLoader {
+
+    final ResourceDictionaries resourceDictionaries;
+
+    final Map<String, Factory<Dimension>> dimensionFactories;
+
+    final FactoryPark<Dimension> dimensionFactoryPark;
+
+    /**
+     * Constructor.
+     *
+     * @param resourceDictionaries  The dictionaries to initialize the industrial park with.
+     * @param dimensionFactories The map of factories for creating dimensions from external config
+     */
+    public LuthierIndustrialPark(
+            ResourceDictionaries resourceDictionaries,
+            Map<String, Factory<Dimension>> dimensionFactories
+    ) {
+        this.resourceDictionaries = resourceDictionaries;
+        this.dimensionFactories = dimensionFactories;
+        Supplier<ObjectNode> dimensionConfig = new ResourceNodeSupplier("DimensionConfig.json");
+        dimensionFactoryPark = new FactoryPark<>(dimensionConfig, dimensionFactories);
+    }
+
+/*
+    LogicalTable getLogicalTable(String tableName);
+    PhysicalTable getPhysicalTable(String tableName);
+    LogicalMetric getLogicalMetric(String metricName);
+    MetricMaker getMetricMaker(String makerName);
+*/
+
+    /**
+     * Retrieve or build a dimension.
+     *
+     * @param dimensionName the name for the dimension to be provided.
+     *
+     * @return the dimension instance corresponding to this name.
+     */
+    Dimension getDimension(String dimensionName) {
+        DimensionDictionary dimensionDictionary = resourceDictionaries.getDimensionDictionary();
+        if (dimensionDictionary.findByApiName(dimensionName) == null) {
+            Dimension dimension = dimensionFactoryPark.buildEntity(dimensionName, this);
+            dimensionDictionary.add(dimension);
+        }
+        return dimensionDictionary.findByApiName(dimensionName);
+    }
+/*
+    SearchProvider getSearchProvider(String searchProviderName);
+    KeyValueStore getKeyValueStore(String keyValueStoreName);
+*/
+
+    @Override
+    public void load() {
+        dimensionFactoryPark.fetchConfig().fieldNames().forEachRemaining(this::getDimension);
+    }
+
+    @Override
+    public DimensionDictionary getDimensionDictionary() {
+        return resourceDictionaries.getDimensionDictionary();
+    }
+
+    @Override
+    public MetricDictionary getMetricDictionary() {
+        return resourceDictionaries.getMetricDictionary();
+    }
+
+    @Override
+    public LogicalTableDictionary getLogicalTableDictionary() {
+        return resourceDictionaries.getLogicalDictionary();
+    }
+
+    @Override
+    public PhysicalTableDictionary getPhysicalTableDictionary() {
+        return getPhysicalTableDictionary();
+    }
+
+    @Override
+    public ResourceDictionaries getDictionaries() {
+        return resourceDictionaries;
+    }
+}
