bda9397580cd25d3a0543881f840e591354a12cc
Andrew Cholewa <acholewa@oath.com>
2019-06-03 16:59:33 -0500

    Fixes typo in `LuthierIndustrialPark`
    -- A typo meant that `getPhysicalTableDictionary()` was invoking itself,
    rather than delegating to `resourceDictionaries`. That has been fixed.

--- luthier/src/main/java/com/yahoo/bard/webservice/config/luthier/LuthierIndustrialPark.java
+++ luthier/src/main/java/com/yahoo/bard/webservice/config/luthier/LuthierIndustrialPark.java
@@ -94,7 +94,7 @@ public class LuthierIndustrialPark implements ConfigurationLoader {
 
     @Override
     public PhysicalTableDictionary getPhysicalTableDictionary() {
-        return getPhysicalTableDictionary();
+        return resourceDictionaries.getPhysicalTableDictionary();
     }
 
     @Override
