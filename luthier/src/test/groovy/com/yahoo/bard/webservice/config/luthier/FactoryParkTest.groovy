8e1ff3330e887d1fb4d27141511f48e2d7c8ed51
Gabriel Luo <gabriel.luo@verizonmedia.com>
2019-06-05 17:13:56 -0500

    modified the .gitignore file so now CI can find the test resource (+1 squashed commit) Squashed commits: [f3a7643d] amending the previous commit

--- luthier/src/test/groovy/com/yahoo/bard/webservice/config/luthier/FactoryParkTest.groovy
+++ luthier/src/test/groovy/com/yahoo/bard/webservice/config/luthier/FactoryParkTest.groovy
@@ -3,12 +3,6 @@ package com.yahoo.bard.webservice.config.luthier
 import spock.lang.Specification
 
 class FactoryParkTest extends Specification {
-    void setup() {
-    }
-
-    void cleanup() {
-    }
-
     def "A FactoryPark<Dimension> correctly builds a Dimension from JSON."() {
     }
 }
