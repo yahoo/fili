25fb3a72f0055a5f55d8a5a586258b1216f994a1
Gabriel Luo <luogm@umich.edu>
2019-08-09 12:22:37 -0500

    changed location, added comments on usage

--- luthier/scripts/luWiki.sh
+++ luthier/scripts/luWiki.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# runs luthier wiki example on port 9012
+cd ../..
 mvn -pl luthier exec:java -Dbard__fili_port=9012 -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 -Dbard__druid_broker=http://localhost:8082/druid/v2
