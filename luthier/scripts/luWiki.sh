249800d4427efb097983fbdba3b6c1d52ba1e7b3
Gabriel Luo <luogm@umich.edu>
2019-08-12 17:43:53 -0500

    updated helper script and added readme

--- luthier/scripts/luWiki.sh
+++ luthier/scripts/luWiki.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# get path to this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# prepares json files
+cd $DIR/../src/main/lua/
+lua config.lua wikiApp
+
+# install fili using mvn
+cd $DIR/../
+mvn clean; mvn install
 # runs luthier wiki example on port 9012
-cd ../..
 mvn -pl luthier exec:java -Dbard__fili_port=9012 -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 -Dbard__druid_broker=http://localhost:8082/druid/v2
---
+++ luthier/src/main/lua/test.sh
@@ -0,0 +1,2 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
