dca49406804b6f8a6562eb18a2ead8744ef72655
Gabriel Luo <luogm@umich.edu>
2019-08-14 13:58:13 -0500

    used <execution> specific <configuration> in pom.xml, must supply exec:java@<id> to target it

--- luthier/scripts/runApp.sh
+++ luthier/scripts/runApp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # runs wiki example
 
-local PORT="${1:-9998}"
-
-#find the root of the project
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PORT=${1:-9012}
 
 #launch the project
-mvn -pl luthier exec:java -Dbard__fili_port=${PORT} -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 -Dbard__druid_broker=http://localhost:8082/druid/v2
+mvn -pl luthier exec:java@run-luthier-main -Dbard__fili_port="$PORT" \
+    -Dbard__druid_coord=http://localhost:8081/druid/coordinator/v1 \
+    -Dbard__druid_broker=http://localhost:8082/druid/v2
