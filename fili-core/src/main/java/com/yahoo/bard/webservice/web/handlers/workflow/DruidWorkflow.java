1c66dabff8dcc1a555b4b322b106c8cf85fc6c9d
Sakshi Haresh Goplani <sakshi.haresh.goplani@oath.com>
2020-10-15 10:38:58 -0500

    Updating import

--- fili-core/src/main/java/com/yahoo/bard/webservice/web/handlers/workflow/DruidWorkflow.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/web/handlers/workflow/DruidWorkflow.java
@@ -28,7 +28,7 @@ import com.yahoo.bard.webservice.web.handlers.TopNMapperRequestHandler;
 import com.yahoo.bard.webservice.web.handlers.VolatileDataRequestHandler;
 import com.yahoo.bard.webservice.web.handlers.WebServiceSelectorRequestHandler;
 import com.yahoo.bard.webservice.web.handlers.WeightCheckRequestHandler;
-import com.yahoo.bard.webservice.web.util.*;
+import com.yahoo.bard.webservice.web.util.QueryWeightUtil;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
