ae8aac8320e9606161df02fd079f8c2843bf03c3
sd-buildbot <dev-null@screwdriver.cd>
2020-11-10 16:14:02 -0600

    Maven return code handling.

--- screwdriver/mvn-build.bash
+++ screwdriver/mvn-build.bash
@@ -11,16 +11,19 @@ mvn -version
 
 # We're not on a release tag, so build and test the code
 echo "INFO mvn Install"
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn install -Pcoverage.build MAVEN_RETURN_CODE=$?
-echo "INFO mvn Site"
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn post-site -Pcoverage.report,sonar
-echo "INFO mvn sonar"
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
-
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn install -Pcoverage.build
+#capture the maven return code
+MAVEN_RETURN_CODE=$?
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
     echo "ERROR Maven verify did not succeed."
     exit ${MAVEN_RETURN_CODE}
 fi
+
+echo "INFO mvn Site"
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn post-site -Pcoverage.report,sonar
+
+echo "INFO mvn sonar"
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
 
 # Only publish whitelisted branches
 WHITELISTED=$(for a in ${PUBLISH_WHITELIST}; do if [ ${TRAVIS_BRANCH} = ${a} ]; then echo true; fi; done;)
