ecf9f5697c92eab1f8ad1edb4e959b39db6a9567
michael-mclawhorn <mclawhor@verizonmedia.com>
2020-11-10 19:35:51 -0600

    fixing tagging (#1107)
    commenting sonar (+9 squashed commits)
    Squashed commits:
    [a1ce99d6] making backup of static objects more explicit (copy ensured) making restoration guaranteed on test suite completion.
    [1c7cfed6] tag pushing
    [ae8aac83] Maven return code handling.
    [8a28ef4b] renaming
    [1f06f112] fixing PR stuff?
    [47946eb7] apt get update?
    [5023c921] Image not template
    [a18c82c5] lua install again?
    [bea58a8d] PR test
    
    Co-authored-by: sd-buildbot <dev-null@screwdriver.cd>

--- screwdriver/mvn-build.bash
+++ screwdriver/mvn-build.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-PUBLISH_WHITELIST="master,screwdriver"
+PUBLISH_WHITELIST="master,sd-master"
 RELEASE_TAG_REGEX="^[0-9]+\\.[0-9]+\\.[0-9]+"
 
 export MAVEN_OPTS="-Xmx3000m"
@@ -22,17 +22,17 @@ fi
 echo "INFO mvn Site"
 mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn post-site -Pcoverage.report,sonar
 
-echo "INFO mvn sonar"
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
+#echo "INFO mvn sonar"
+#mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
 
 # Only publish whitelisted branches
-WHITELISTED=$(for a in ${PUBLISH_WHITELIST}; do if [ ${TRAVIS_BRANCH} = ${a} ]; then echo true; fi; done;)
+WHITELISTED=$(for a in ${PUBLISH_WHITELIST}; do if [[ ${TRAVIS_BRANCH} == ${a} ]]; then echo true; fi; done;)
 if [[ "${WHITELISTED}" != "true" ]]; then
     echo "INFO Do not flag for publication, this is not a whitelisted branch: ${TRAVIS_BRANCH}"
     exit 0
 fi
 
-# Don't publish PR builds
+# Don't publish PR builds.
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     echo "INFO Do not flag for publication, this is a PR: ${TRAVIS_PULL_REQUEST}"
     exit 0
