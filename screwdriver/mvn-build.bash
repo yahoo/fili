bd7ae11fb3365b37893dd7a5ed65d168585ce0fc
michael-mclawhorn <mclawhor@verizonmedia.com>
2020-11-10 18:54:50 -0600

    PR test (#1106)
    Triggering the commit workflow on screwdriver.

--- screwdriver/mvn-build.bash
+++ screwdriver/mvn-build.bash
@@ -10,13 +10,20 @@ export FILI_TEST_LIST=a,2,bc,234
 mvn -version
 
 # We're not on a release tag, so build and test the code
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn install -Pcoverage.build MAVEN_RETURN_CODE=$?
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn post-site -Pcoverage.report,sonar
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
+echo "INFO mvn Install"
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn install -Pcoverage.build
+#capture the maven return code
+MAVEN_RETURN_CODE=$?
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
     echo "ERROR Maven verify did not succeed."
     exit ${MAVEN_RETURN_CODE}
 fi
+
+echo "INFO mvn Site"
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn post-site -Pcoverage.report,sonar
+
+echo "INFO mvn sonar"
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn sonar:sonar -Dsonar.projectKey=yahoo_fili -Dsonar.organization=yahoo -Dsonar.login=${SONAR_TOKEN} -Pcoverage.report,sonar
 
 # Only publish whitelisted branches
 WHITELISTED=$(for a in ${PUBLISH_WHITELIST}; do if [ ${TRAVIS_BRANCH} = ${a} ]; then echo true; fi; done;)
@@ -30,7 +37,3 @@ if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     echo "INFO Do not flag for publication, this is a PR: ${TRAVIS_PULL_REQUEST}"
     exit 0
 fi
-
-# This is a publishable push, update the tag to trigger a deploy
-echo "Ci tagging: "
-screwdriver/ci-tag.bash
