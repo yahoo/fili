8983c86d13ae7f0ceeb842ef1e5c041abf1e81b0
sd-buildbot <dev-null@screwdriver.cd>
2020-11-11 18:10:30 -0600

    Tweaking to avoid circular invocation

--- screwdriver/bintray-publish.bash
+++ screwdriver/bintray-publish.bash
@@ -1,32 +1,32 @@
 #!/bin/bash
-PUBLISH_WHITELIST="master"
-RELEASE_TAG_REGEX="^[0-9]+\\.[0-9]+\\.[0-9]+"
-
 export MAVEN_OPTS="-Xmx3000m"
 
 # This is a tag event with a version style tag
 echo "INFO Publishing, this is a github release event"
-#get the last tag on this branch
-LAST_TAG=$(git describe)
-echo "INFO Last tag: $LAST_TAG"
 
+#get the last tag on this branch
+echo "INFO Last tag: $(git describe)"
 
 echo "INFO Maven versions set : "
 mvn versions:set -DnewVersion=$(git describe) -DgenerateBackupPoms=false
 MAVEN_RETURN_CODE=$?
+
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
     echo "ERROR Unable to set version via Maven. Aborting publication."
     exit ${MAVEN_RETURN_CODE}
 fi
+
 mvn versions:update-property -Dproperty=version.fili -DnewVersion=$(git describe) -DgenerateBackupPoms=false
 MAVEN_RETURN_CODE=$?
+
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
     echo "ERROR Unable to update property via Maven. Aborting publication."
     exit ${MAVEN_RETURN_CODE}
 fi
 
-echo "INFO Deploying: "
-mvn deploy --settings screwdriver/bintray-settings.xml
+echo "INFO Deploying to bintray: "
+mvn -DskipTests deploy --settings screwdriver/bintray-settings.xml
+
 MAVEN_RETURN_CODE=$?
 if [[ ${MAVEN_RETURN_CODE} -ne 0 ]]; then
     echo "ERROR Publication failed."
---
+++ screwdriver/is_whitelisted.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+PUBLISH_WHITELIST="master sd-master"
+
+# Only publish whitelisted branches
+echo "Branch: ${GIT_BRANCH} Whitelist: ${PUBLISH_WHITELIST}"
+WHITELISTED="false"
+for a in ${PUBLISH_WHITELIST}; do
+    if [[ ${GIT_BRANCH} = ${a} ]];
+    then
+      WHITELISTED="true";
+    fi;
+done;
+echo Whitelisted: $WHITELISTED
+[[ ${WHITELISTED} == "true" ]];
