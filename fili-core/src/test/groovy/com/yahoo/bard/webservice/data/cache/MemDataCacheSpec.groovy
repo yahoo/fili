d7097f9a060c262036cd48edef665aeacde39c6a
Bharat Motwani <>
2020-11-19 12:26:51 -0600

    fix tests

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/data/cache/MemDataCacheSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/data/cache/MemDataCacheSpec.groovy
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeoutException
 
 class MemDataCacheSpec extends Specification {
     MemDataCache memDataCache;
-    MemcachedClient client;
 
     void setup() {
         SYSTEM_CONFIG.setProperty(MemDataCache.OPERATION_TIMEOUT_CONFIG_KEY, "12345")
@@ -25,7 +24,7 @@ class MemDataCacheSpec extends Specification {
     void cleanup() {
         SYSTEM_CONFIG.clearProperty(MemDataCache.OPERATION_TIMEOUT_CONFIG_KEY)
         SYSTEM_CONFIG.clearProperty(MemDataCache.EXPIRATION_KEY)
-        SYSTEM_CONFIG.setProperty(MemDataCache.WAIT_FOR_FUTURE)
+        SYSTEM_CONFIG.clearProperty(MemDataCache.WAIT_FOR_FUTURE)
     }
 
     def "Constructor has correct expiration and timeout"() {
@@ -53,7 +52,7 @@ class MemDataCacheSpec extends Specification {
 
     def "when #waitForFuture is set to false , It doesn't wait for future anymore"() {
 
-        SYSTEM_CONFIG.setProperty(MemDataCache.WAIT_FOR_FUTURE, waitForFuture as String)
+        SYSTEM_CONFIG.setProperty(MemDataCache.WAIT_FOR_FUTURE, "false")
         OperationFuture future = Mock(OperationFuture)
         MemcachedClient client = Mock(MemcachedClient)
         memDataCache = new MemDataCache(client);
@@ -65,27 +64,14 @@ class MemDataCacheSpec extends Specification {
         0 * future.get()
         result
 
-        where:
-        waitForFuture << false
-
-    }
-
-    def "when #waitForFuture is set to true , It waits for timeout from future object"() {
-
-        SYSTEM_CONFIG.setProperty(MemDataCache.WAIT_FOR_FUTURE, waitForFuture as String)
-        OperationFuture future = Mock(OperationFuture)
-        MemcachedClient client = Mock(MemcachedClient)
-        memDataCache = new MemDataCache(client);
+        SYSTEM_CONFIG.setProperty(MemDataCache.WAIT_FOR_FUTURE, "true")
         when:
-        boolean result = memDataCache.setInSeconds("key", "value",1000)
+        boolean result1 = memDataCache.setInSeconds("key", "value",1000)
 
         then:
         1 * client.set("key", 1000, "value") >> {throw new TimeoutException()} >> future
         thrown(IllegalStateException)
-        !result
-
-        where:
-        waitForFuture << true
+        !result1
 
     }
 }
