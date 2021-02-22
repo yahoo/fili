212305f79f9701f9cab53d4340464379ef5ed6d4
Chandrasekar-Rajasekar <chandrasekar.rajasekar@gmail.com>
2021-02-22 10:57:48 -0600

    If the request context return null uribuilder, do not add pagination links (#1136)
    * If the request context return null uribuilder, do not add pagination links
    
    * Added changlog.
    Changed javadoc to reflect optional nature of UriBuilder
    
    Co-authored-by: Chandrasekar Rajasekar <chandrasekar.rajasekar@oath.com>
    Co-authored-by: sd-buildbot <dev-null@screwdriver.cd>

--- fili-core/src/main/java/com/yahoo/bard/webservice/web/handlers/RequestContext.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/web/handlers/RequestContext.java
@@ -26,7 +26,7 @@ public class RequestContext {
     /**
      * Build a context for a request.
      *
-     * @param containerRequestContext  context from the http request object
+     * @param containerRequestContext  context from the http request object (nullabe in embedded contexts)
      * @param readCache  true if the cache should be checked for a response
      */
     public RequestContext(ContainerRequestContext containerRequestContext, boolean readCache) {
@@ -76,6 +76,8 @@ public class RequestContext {
     }
 
     public UriBuilder getUriBuilder() {
-        return containerRequestContext.getUriInfo().getRequestUriBuilder();
+        return containerRequestContext == null
+                ? null
+                : containerRequestContext.getUriInfo().getRequestUriBuilder();
     }
 }
