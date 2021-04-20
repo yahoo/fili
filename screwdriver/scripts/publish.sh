9cb9bdc8e7661b8d3905d6bc08c335403d265189
Sakshi Haresh Goplani <sakshi.haresh.goplani@oath.com>
2021-04-20 12:11:18 -0700

    Working release to Nexus Repository Manager

--- screwdriver/scripts/publish.sh
+++ screwdriver/scripts/publish.sh
@@ -8,8 +8,8 @@ export GPG_TTY=$(tty)
 mkdir -p screwdriver/deploy
 chmod 0700 screwdriver/deploy
 
-openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/pubring.gpg.enc -out screwdriver/deploy/pubring.gpg -d
-openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/secring.gpg.enc -out screwdriver/deploy/secring.gpg -d
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/pubring.gpg.enc -out screwdriver/deploy/pubring.gpg -pbkdf2 -d
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/secring.gpg.enc -out screwdriver/deploy/secring.gpg -pbkdf2 -d
 
 mvn -B deploy -P ossrh -Dmaven.test.skip=true --settings screwdriver/settings/settings-publish.xml
 
